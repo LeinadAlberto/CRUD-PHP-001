bfbf956c36949a3255143fd4c0ed841dad93a9ab
Alberto <dany.canaviri2022@gmail.com>
2023-03-30 11:26:49 -0400

    30-03-2023 Template Modularizado

---
+++ mainleftpanel.php
@@ -0,0 +1,30 @@
+<!-- ########## START: LEFT PANEL ########## -->
+<div class="br-logo"><a href=""><span>[</span>CRUD - PHP8<span>]</span></a></div>
+
+<div class="br-sideleft overflow-y-auto">
+  <label class="sidebar-label pd-x-15 mg-t-20">Navegación</label>
+
+  <div class="br-sideleft-menu">
+    <a href="../Home/" class="br-menu-link">
+      <div class="br-menu-item">
+        <i class="menu-item-icon icon ion-ios-home-outline tx-22"></i>
+        <span class="menu-item-label">Panel</span>
+      </div><!-- menu-item -->
+    </a><!-- br-menu-link -->
+
+        <!-- Mantenimiento -->
+    <a href="#" class="br-menu-link">
+      <div class="br-menu-item">
+        <i class="menu-item-icon icon ion-ios-filing-outline tx-24"></i>
+        <span class="menu-item-label">Mantenimiento</span>
+        <i class="menu-item-arrow fa fa-angle-down"></i>
+      </div><!-- menu-item -->
+    </a><!-- br-menu-link -->
+
+        <!-- Producto -->
+    <ul class="br-menu-sub nav flex-column">
+      <li class="nav-item"><a href="../MntProducto/" class="nav-link">Producto</a></li>
+    </ul>
+  </div><!-- br-sideleft-menu -->
+</div><!-- br-sideleft -->
+<!-- ########## END: LEFT PANEL ########## -->
